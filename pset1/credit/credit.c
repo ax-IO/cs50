4d6e1dd786cd230419bd44f9f8ef41dd76550d5e
Gauthier Germain <g.germain98@gmail.com>
2017-11-07 10:45:31 +0000

    prototype

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -2,20 +2,71 @@
 #include <stdio.h>
 
 //Implémentation de fonction
+int longueur (long long n);
+int checksum (long long n);
+int digit (long long n, int k);
 
+
+///////////FONCTION PRINCIPALE////////////////
 
 int main (void)
 {
-//Déclaration variables
-    int  ;
+    long long number = get_long_long("Number: ");
 
-}
-//définition fonction repeat
-//ne retourne rien (effets secondaires)
-void repeat (char c, int n)
-{
-    for (int i = 0; i < n; i++)
+    int nbe_digit = longueur (number);
+    printf("le nombre comporte %d digits !\n", nbe_digit);
+
+//Test du checksum
+    if (checksum (number) % 10 != 0)
     {
-        printf("%c", c);
+        if (nbe_digit == 15 && digit (number, 1) == 3)
+        {
+            ;
+        }
     }
+    else
+    {
+        ;
+    }
+    return 0;
+}
+
+
+
+///////////FONCTIONS////////////////
+
+
+//retourne le nombre de digits
+int longueur (long long n)
+{
+    int l = 0;
+    while (n >= 10)
+    {
+        n/=10;
+        l++;
+    }
+    return l+1;
+}
+
+
+//retourne le nombre obtenu par le Luhn’s algorithm
+int checksum (long long n)
+{
+    int l = longueur (n);
+    if (l % 2 == 0)
+    {
+        return 0; //cas ou le nombre est pair
+    }
+    else
+    {
+        return 1; // cas où le nombre est impair
+    }
+}
+
+//retourne le k digit en partant du début
+int digit (long long n, int k)
+{
+    int l = longueur (n) ;
+    printf("k=%d et %d",k,l);
+    return 0;
 }
